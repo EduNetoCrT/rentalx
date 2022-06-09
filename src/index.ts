9435615b65cbc35c14b179f82dd500c643cac525
Eduardo Neto <eduardo.js_jp@hotmail.com>
2022-06-09 17:54:31 -0300

    model, repositories e routrs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import express from 'express';
+import { categoriesRoutes } from './routers/categories.routes';
 
+const app = express();
+const port = 3300;
+
+app.use(express.json());
+app.use(categoriesRoutes);
+
+app.post('/cursos', (request, response) => {
+    const {name} = request.body;
+
+    return response.json({name})
+})
+app.use((req, res) =>{
+    res.json({msg:"Endpoint não encontrado"})
+});
+
+app.listen(port, () => console.log(`Servidor rodando na porta, ${port}`));
